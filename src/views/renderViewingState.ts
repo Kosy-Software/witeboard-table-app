d2bbe411fce8b59a954bc24e0d1bde19fd127cb7
Sam Smets <samsmets@hotmail.com>
2021-06-02 13:18:59 +0800

    Fixed issue with menu overlapping at the bottom

--- src/views/renderViewingState.ts
+++ src/views/renderViewingState.ts
@@ -3,7 +3,7 @@ export function renderViewingState(state: ComponentState): HTMLElement {
     let viewingRoot = document.querySelector("#viewing") as HTMLTemplateElement;
     let viewingElement = viewingRoot.content.firstElementChild.cloneNode(true) as HTMLElement;
     let iframe = viewingElement.querySelector("iframe") as HTMLIFrameElement;
-    iframe.style.height = "100vh";
+    iframe.style.height = `calc(100vh - 36px)`;
     iframe.style.width = "100vw";
     iframe.src = `https://witeboard.com/${state.tableName}`;
 
